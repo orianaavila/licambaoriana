1652f3073b99231948f682bf43f1727037ade0e4
orianaavila <50601441+orianaavila@users.noreply.github.com>
2019-08-14 16:26:50 -0500

    aqui esta la tarea

---
+++ licamba/Licamba/FormLogin.Designer.cs
@@ -0,0 +1,147 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: PABLO AGUILAR
+ * Fecha: 05/06/2019
+ * Hora: 09:41 p.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace LicambaCamilaPachon
+{
+	partial class FormLogin
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button Ingresar;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+			this.Ingresar = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Ingresar
+			// 
+			this.Ingresar.Location = new System.Drawing.Point(84, 261);
+			this.Ingresar.Name = "Ingresar";
+			this.Ingresar.Size = new System.Drawing.Size(85, 23);
+			this.Ingresar.TabIndex = 0;
+			this.Ingresar.Text = "Ingresar";
+			this.Ingresar.UseVisualStyleBackColor = true;
+			this.Ingresar.Click += new System.EventHandler(this.IngresarClick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.White;
+			this.textBox1.Location = new System.Drawing.Point(23, 204);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(216, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(23, 137);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(216, 22);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(23, 111);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Usuario:";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(23, 178);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Contraseña:";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(94, 300);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(156, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "¿Olvido la contraseña?";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.DarkGreen;
+			this.pictureBox1.ErrorImage = null;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.InitialImage = null;
+			this.pictureBox1.Location = new System.Drawing.Point(84, 25);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(85, 83);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.PictureBox1Click);
+			// 
+			// FormLogin
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.DarkGreen;
+			this.ClientSize = new System.Drawing.Size(262, 332);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.Ingresar);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FormLogin";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Ingreso";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ licamba/Licamba/FormLogin.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: PABLO AGUILAR
+ * Fecha: 05/06/2019
+ * Hora: 09:41 p.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LicambaCamilaPachon
+{
+	/// <summary>
+	/// Description of FormLogin.
+	/// </summary>
+	public partial class FormLogin : Form
+	{
+		public FormLogin()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void PictureBox1Click(object sender, EventArgs e)
+		{
+	
+		}
+		void IngresarClick(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
